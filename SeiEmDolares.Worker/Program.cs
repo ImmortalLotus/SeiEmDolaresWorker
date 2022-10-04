7c425aad9a4d6aea138a1b41520c7c266c30de27
06914269963 <joaotelles@setic.ro.gov.br>
2022-10-04 11:23:26 -0400

    MUITAS mudançasno worker

--- SeiEmDolares.Worker/Program.cs
+++ SeiEmDolares.Worker/Program.cs
@@ -1,13 +1,15 @@
 using Sei.Infra.Repository;
 using Sei.Infra.RepositoryInterface;
+using SeiEmDolares.Infra.Repository;
 using SeiEmDolares.Infra.RepositoryInterface;
 using SeiEmDolares.Worker;
+
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddTransient<IProtocoloRepository, ProtocoloRepository>();
-        services.AddTransient<IProtocoloNoSeiEmDolaresRepository,IProtocoloNoSeiEmDolaresRepository>();
+        services.AddTransient<IProtocoloNoSeiEmDolaresRepository,ProtocoloNoSeiEmDolaresRepository>();
         services.AddHostedService<Worker>();
     })
     .Build();
